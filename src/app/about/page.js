d5141743b409ff5c2f8e77e63b4c38b1e39cbb0d
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2023-03-06 23:53:37 +0100

    About page created

---
+++ src/app/about/page.js
@@ -0,0 +1,38 @@
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About Page</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting 
+        industry. Lorem Ipsum has been the industrys standard dummy text 
+        ever since the 1500s, when an unknown printer took a galley of type 
+        and scrambled it to make a type specimen book. 
+      </p>
+      <p>
+        It has survived not only five centuries, but also the leap into 
+        electronic typesetting, remaining essentially unchanged. It was 
+        popularised in the 1960s with the release of Letraset sheets 
+        Lorem Ipsum is simply dummy text of the printing and typesetting 
+        industry. Lorem Ipsum has been the industrys standard dummy text 
+        ever since the 1500s, when an unknown printer took a galley of type 
+        and scrambled it to make a type specimen book. 
+        Lorem Ipsum is simply dummy text of the printing and typesetting 
+        industry. Lorem Ipsum has been the industrys standard dummy text 
+        ever since the 1500s, when an unknown printer took a galley of type 
+        and scrambled it to make a type specimen book. 
+      </p>
+
+      <p>
+        containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        Lorem Ipsum is simply dummy text of the printing and typesetting 
+        industry. Lorem Ipsum has been the industrys standard dummy text 
+        ever since the 1500s, when an unknown printer took a galley of type 
+        and scrambled it to make a type specimen book. 
+      </p>
+    </div>
+  )
+}
+
+
+export default About;
